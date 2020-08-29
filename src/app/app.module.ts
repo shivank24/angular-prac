bc1624a917769813a16b31e9b569a3c8d9c1de83
Shivank <skj9519@gmail.com>
2020-08-30 01:16:52 +0530

    4.0-Component, service, data handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { TestCompComponent } from './test-comp/test-comp.component';
+import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService} from './authors.service';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCompComponent,
+    CoursesComponent,
+    AuthorsComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/courses.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { CoursesService} from './courses.service';
+
+@Component({
+    selector: 'courses',
+    template:
+    `
+    <h2>{{"Title:" + getTitle() }}</h2>
+
+    <ul>
+        <li *ngFor="let course of courses"> 
+            {{ course }}
+        </li>
+    </ul>
+
+    <img src="{{ imageUrl }}"/>
+    <img [src]="imageUrl"/>
+    <p></p>
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>
+    <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue': 'red'">Save</button>
+    <div (click)="onDivClick()">
+        <button (click)="onClick($event)">Save</button>
+    </div>
+    <input [(ngModel)]='email' (keyup.enter)="onKeyUp()"/>
+    <br/>
+    {{ course.title | uppercase | lowercase}} <br/>
+    {{ course.students | number}} <br/>
+    {{ course.rating | number:'1.2-3'}} <br/>
+    {{ course.price | currency:'AUD':true:'3.1-2'}} <br/>
+    {{ course.releaseDate | date:'shortDate' }}
+    <br/>
+    {{ text | summary }}
+    `
+})
+export class CoursesComponent{
+    title = "List of courses";
+    courses;
+    imageUrl = "http://lorempixel.com/400/200/";
+    colSpan=2;
+    isActive = false;
+    email = 'me@example.com';
+    course = {
+        title: 'Angular course',
+        rating: 4.9745,
+        students: 30123,
+        price: 190.85,
+        releaseDate: new Date(2016,1,14)
+    };
+    text = "Lorem fkeik cjbeakncol jbclkwnjec bjelcnwjenclwe cjwbelc"
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses()
+    }
+
+    getTitle(){
+        return this.title
+    }
+    onClick($event){
+        $event.stopPropagation()
+        console.log('Button clicked')
+        console.log($event)
+        
+    }
+    onDivClick(){
+        console.log('Div clicked')
+    }
+    onKeyUp(){
+        console.log(this.email)
+    }
+}
+
+//Text from courses-component
