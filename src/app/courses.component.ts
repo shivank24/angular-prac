00d7ee6a1db4bc443672740470cc36a5fdd7272c
Shivank <skj9519@gmail.com>
2021-11-10 18:03:50 +0530

    PART 4: Displaying data and handling events

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -12,12 +12,59 @@ import { CoursesService } from './courses.service';
         </li>
         
     </ul>
-    
+
+    <img src="{{ imageUrl }}"/>
+    <img [src]="imageUrl" />                    <!--#property binding-->
+
+
+    <table>
+        <tr>
+            <td [attr.colspan]="colSpan"></td>          <!--attribute binding-->
+        </tr>
+    </table>
+
+
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>         <!-- class binding -->
+    <button [style.backgroundColor]="isActive ? 'blue': 'white' ">Save</button>     <!-- style binding -->
+    <br>
+
+    <div (click)="onDivClicked()">                                                  <!-- event bubbling -->
+        <button (click)="onSave($event)" >Save</button>                             <!-- event binding -->
+    </div>
+
+    <br>
+
+    <input #email (keyup.enter)="onKeyUp(email.value)" />                           <!--event filtering, template variable -->
+    <input [(ngModel)]="newEmail" (keyup.enter)="onEnter()"/>                       <!-- two way binding -->
+    <br>
+
+    {{course.title | uppercase | lowercase}}<br>
+    {{course.students | number}} <br>
+    {{course.rating | number:'2.1-1'}} <br>
+    {{course.price | currency:'AUD':true:'3.2-2'}} <br>
+    {{course.releaseDate | date:'shortDate'}} <br>
+
+    {{text | summary:50}}
+
     `
 })
 export class CoursesComponent{
     title = "List of courses";
     courses;
+    imageUrl = 'http://lorempixel.com/400/200/'
+    colSpan=2;
+    isActive=true;
+    newEmail="me@example.com";
+
+    course = {
+        title: "The Complete Angular",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016,3,1)
+    }
+
+    text = "ambcjhebckjca cbjbceucibeabckae caeoccacl"
 
     constructor(service: CoursesService){
         this.courses = service.getCourses()
@@ -25,6 +72,23 @@ export class CoursesComponent{
 
     getTitle(){
         return "List of many courses"
+    }
+
+    onSave($event){
+        $event.stopPropagation();                                               // stop event bubbling
+        console.log('button was clicked', $event);
+    }
+
+    onDivClicked(){
+        console.log('Div  was clicked');
+    }
+
+    onKeyUp(email){
+        console.log('Enter was pressed:', email)
+    }
+
+    onEnter(){
+        console.log(this.newEmail)
     }
     
     // setTimeout(()=>{},10);
---
+++ src/app/summary.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'summary'
+})
+export class SummaryPipe implements PipeTransform{
+    transform(value: string, limit?:number, another?:any){
+
+        let actualLimit = limit ? limit : 50;
+
+        if (!value)
+            return null;
+
+        return value.substr(0, actualLimit) + '...'
+
+    }
+}
