60df41623a5c48a875e05e1087f4623a865b305e
Shivank <skj9519@gmail.com>
2021-12-01 18:07:40 +0530

    PART 7: Template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PanelComponent } from './panel/panel.component';
 import { LikesComponent } from './likes/likes.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ZippyComponent } from './zippy/zippy.component';
     PanelComponent,
     LikesComponent,
     InputFormatDirective,
-    ZippyComponent
+    ZippyComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
